f0368b5c2173e09c1474ee839e5e6e5cd158745f
Gustavo Silva <gustavo.sillva.lara@gmail.com>
2026-04-08 06:44:20 -0300

    Ajutes

--- src/PrimeirosPassos/Capitulo04/BancoBasico.java
+++ src/PrimeirosPassos/Capitulo04/BancoBasico.java
@@ -31,36 +31,42 @@ public class BancoBasico {
                     """);
             System.out.print("Opção desejada: ");
             int opcao = sc.nextInt();
-            switch (opcao){
-                case 1:
-                    System.out.printf("Saldo atual: %.2f\n", saldoConta);
-                    continue;
+            if(opcao == 4){
+                System.out.println("Encerrando o programa...");
+                break;
+            }else {
+                switch (opcao) {
+                    case 1:
+                        System.out.printf("Saldo atual: %.2f\n", saldoConta);
+                        break;
+
                     case 2:
                         System.out.print("Informe o valor para depositar: ");
                         double deposito = sc.nextDouble();
-                        if(deposito < 0) {
-                            System.out.println("Não é possivel depositar valores negativos!!!");
-                        }else{
+                        if (deposito < 0) {
+                            System.out.println("Não é possível depositar valores negativos!!!");
+                        } else {
                             saldoConta += deposito;
-                            System.out.printf("Saldo atual: %.2f", saldoConta);
+                            System.out.printf("Saldo atualizado: %.2f\n", saldoConta);
                         }
-                        continue;
-                        case 3:
-                            System.out.print("Informe o valor para transferir: ");
-                            double transferir = sc.nextDouble();
-                            if(saldoConta < transferir) {
-                                System.out.println("Saldo insuficiente!!!");
-                            }else {
-                                saldoConta -= transferir;
-                                System.out.println("Transferido com sucesso!!");
-                                System.out.printf("Saldo atual: %.2f", saldoConta);
-                            }
-                            continue;
-                            case 4:
-                                System.out.println("Encerrando o programa!!!");
-                                break;
-                                default:
-                                    System.out.println("Digite uma das opções acima!!!");
+                        break;
+
+                    case 3:
+                        System.out.print("Informe o valor para transferir: ");
+                        double transferir = sc.nextDouble();
+                        if (transferir > saldoConta) {
+                            System.out.println("Saldo insuficiente!!!");
+                        } else {
+                            saldoConta -= transferir;
+                            System.out.println("Transferido com sucesso!!");
+                            System.out.printf("Saldo atual: %.2f\n", saldoConta);
+                        }
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida! Digite uma das opções do menu.");
+                        break;
+                }
             }
         }
     }
